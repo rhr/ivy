d9237e3e7f5895651e5aded5ec22e65e872a017d
Rick Ree <github@rree.fastmail.fm>
2013-10-08 16:19:53 -0500

    read trees from file; assign colors based on order in file

--- examples/treegraph-test/merge-strees.py
+++ examples/treegraph-test/merge-strees.py
@@ -3,31 +3,10 @@ from ivy import treegraph as tg
 from collections import defaultdict
 import graph_tool.all as gt
 
-def fetch_stree(stree_id):
-    print 'fetching stree', stree_id, '...',
-    u = 'http://reelab.net/phylografter/stree/newick.txt/%s' % stree_id
-    lfmt='snode.id,ottol_name.ncbi_taxid,otu.label'
-    p = dict(lfmt=lfmt, ifmt='snode.id')
-    resp = requests.get(u, params=p)
-    r = ivy.tree.read(resp.content)
-    r.stree = stree_id
-    r.ladderize()
-    ivy.tree.index(r)
-    for n in r:
-        if n.isleaf:
-            v = n.label.split('_')
-            n.snode_id = int(v[0])
-            n.taxid = int(v[1]) if (len(v)>1 and
-                                    v[1] and v[1] != 'None') else None
-            #n.label = '_'.join(v[2:])
-        else:
-            n.snode_id = int(n.label)
-    print 'done'
-    return r
-
 g = tg.load_taxonomy_graph('ncbi/ncbi.xml.gz')
 
 strees = []
+i = 1
 with open('strees.newick') as f:
     for line in f:
         r = ivy.tree.read(line)
@@ -40,31 +19,33 @@ with open('strees.newick') as f:
                                         v[1] and v[1] != 'None') else None
             else:
                 n.snode_id = int(n.label)
+        r.stree = i
         strees.append(r)
+        i += 1
 
 stree2color = {}
 for i, r in enumerate(strees):
-    stree2color[stree] = tg.color20[i % 20]
+    stree2color[r.stree] = tg.color20[i % 20]
 
 taxids = set()
 for r in strees:
     tg.map_stree(g, r)
     for lf in r.leaves(): taxids.update(lf.taxid_rootpath)
 
-g = tg.taxid_new_subgraph(g, taxids)
+taxg = tg.taxid_new_subgraph(g, taxids)
 
-verts = g.new_vertex_property('bool')
-edges = g.new_edge_property('bool')
-for r in roots:
-    tg.merge_stree(g, r, r.stree, verts, edges)
+verts = taxg.new_vertex_property('bool')
+edges = taxg.new_edge_property('bool')
+for r in strees:
+    tg.merge_stree(taxg, r, r.stree, verts, edges)
  
-root = g.root
+root = taxg.root
 
-gv = tg.graph_view(g, vfilt=verts, efilt=edges)
+gv = tg.graph_view(taxg, vfilt=verts, efilt=edges)
 
 for x in gv.vertices():
-    if x.in_degree()==0 and int(x)!=int(g.root):
-        v = g.vertex(int(x))
+    if x.in_degree()==0 and int(x)!=int(taxg.root):
+        v = taxg.vertex(int(x))
         while 1:
             e = v.in_edges().next()
             edges[e] = 1
@@ -77,48 +58,48 @@ for x in gv.vertices():
 # the following code sets up the visualization
 ecolor = gv.new_edge_property('string')
 for e in gv.edges():
-    est = g.edge_strees[e]
+    est = taxg.edge_strees[e]
     if len(est)==1: ecolor[e] = stree2color[est[0]]
     else: ecolor[e] = 'gray'
 
 ewidth = gv.new_edge_property('int')
 for e in gv.edges():
-    est = g.edge_strees[e]
+    est = taxg.edge_strees[e]
     if len(est): ewidth[e] = 1+len(est)
     else: ewidth[e] = 1
 
 vcolor = gv.new_vertex_property('string')
 for v in gv.vertices(): vcolor[v] = 'gray'
 for v in gv.vertices():
-    if g.vertex_in_taxonomy[v]: vcolor[v] = 'green'
+    if taxg.vertex_in_taxonomy[v]: vcolor[v] = 'green'
     else:
-        vcolor[v] = stree2color[g.vertex_strees[v][0]]
-        if len(g.vertex_strees[v])>1:
+        vcolor[v] = stree2color[taxg.vertex_strees[v][0]]
+        if len(taxg.vertex_strees[v])>1:
             vcolor[v] = 'yellow'
-    s = (g.vertex_name[v] or '').lower()
+    s = (taxg.vertex_name[v] or '').lower()
 
 vsize = gv.new_vertex_property('int')
 for v in gv.vertices():
-    n = len(g.vertex_strees[v])
+    n = len(taxg.vertex_strees[v])
     if n: vsize[v] = n+2
     else: vsize[v] = 1
 
 vtext = gv.new_vertex_property('string')
 for v in gv.vertices():
     if v.out_degree():
-        s = g.vertex_name[v]
+        s = taxg.vertex_name[v]
         vtext[v] = s
 
 dprops = gv.new_vertex_property('string')
 for v in gv.vertices():
-    st = str(list(g.vertex_strees[v]))
-    s = g.vertex_name[v]
+    st = str(list(taxg.vertex_strees[v]))
+    s = taxg.vertex_name[v]
     if not s:
-        s = str([ g.taxid_name(x) for x in g.vertex_stem_cdef[v] ])
+        s = str([ taxg.taxid_name(x) for x in taxg.vertex_stem_cdef[v] ])
     s = '%s: %s' % (st, s)
     dprops[v] = s
     
-pos, pin = tg.layout(g, gv, root, sfdp=True, deg0=190.0,
+pos, pin = tg.layout(taxg, gv, root, sfdp=True, deg0=190.0,
                      degspan=160.0, radius=1000)
 
 gt.interactive_window(
