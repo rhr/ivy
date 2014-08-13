61363d3774955c5de34fa85dc7f2a93c7bb92896
rhr <github@rree.fastmail.fm>
2014-08-13 14:47:17 -0500

    new example script for merging trees with a taxonomy

---
+++ examples/treegraph-test/merge-phyloboost-trees.py
@@ -0,0 +1,128 @@
+import ivy, requests
+from ivy import treegraph as tg
+from collections import defaultdict
+import graph_tool.all as gt
+
+g = tg.load_taxonomy_graph('ncbi/ncbi.xml.gz')
+
+strees = []
+i = 1
+with open('test_trees.tre') as f:
+    for line in f:
+        tree = line.split("\t")[3]
+        print tree
+        r = ivy.tree.read(tree)
+        ivy.tree.index(r)
+        for n in r:
+            if n.isleaf:
+                v = n.label.split('_')
+                n.snode_id = int(v[0][2:])
+                n.taxid = int(v[1][2:]) if (len(v)>1 and
+                                        v[1] and v[1] != 'None') else None
+            else:
+                n.snode_id = int(n.id)
+        r.stree = i
+        strees.append(r)
+        i += 1
+
+stree2color = {}
+for i, r in enumerate(strees):
+    stree2color[r.stree] = tg.color20[i % 20]
+
+taxids = set()
+for r in strees:
+    tg.map_stree(g, r)
+    for lf in r.leaves(): taxids.update(lf.taxid_rootpath)
+
+root_taxa = set([ r.taxid for r in strees ])
+if len(root_taxa) > 1:
+    rps = [ tg.taxid_rootpath(g, x) for x in root_taxa ]
+    mrca_taxid = tg.rootpath_mrca(rps)
+    for x in rps:
+        taxids.update(x[:x.index(mrca_taxid)+1])
+
+taxg = tg.taxid_new_subgraph(g, taxids)
+
+verts = taxg.new_vertex_property('bool')
+edges = taxg.new_edge_property('bool')
+for r in strees:
+    tg.merge_stree(taxg, r, r.stree, verts, edges)
+ 
+root = taxg.root
+
+gv = tg.graph_view(taxg, vfilt=verts, efilt=edges)
+
+for x in gv.vertices():
+    if x.in_degree()==0 and int(x)!=int(taxg.root):
+        v = taxg.vertex(int(x))
+        while 1:
+            try:
+                e = v.in_edges().next()
+            except StopIteration:
+                break
+            edges[e] = 1
+            p = e.source()
+            if verts[p]: break
+            else:
+                verts[p] = 1
+                v = p
+
+# the following code sets up the visualization
+ecolor = gv.new_edge_property('string')
+for e in gv.edges():
+    est = taxg.edge_strees[e]
+    if len(est)==1: ecolor[e] = stree2color[est[0]]
+    else: ecolor[e] = 'gray'
+
+ewidth = gv.new_edge_property('int')
+for e in gv.edges():
+    est = taxg.edge_strees[e]
+    if len(est): ewidth[e] = 1+len(est)
+    else: ewidth[e] = 1
+
+vcolor = gv.new_vertex_property('string')
+for v in gv.vertices(): vcolor[v] = 'gray'
+for v in gv.vertices():
+    if taxg.vertex_in_taxonomy[v]: vcolor[v] = 'green'
+    else:
+        vcolor[v] = stree2color[taxg.vertex_strees[v][0]]
+        if len(taxg.vertex_strees[v])>1:
+            vcolor[v] = 'yellow'
+    s = (taxg.vertex_name[v] or '').lower()
+
+vsize = gv.new_vertex_property('int')
+for v in gv.vertices():
+    n = len(taxg.vertex_strees[v])
+    if n: vsize[v] = n+2
+    else: vsize[v] = 1
+
+vtext = gv.new_vertex_property('string')
+for v in gv.vertices():
+    if v.out_degree():
+        s = taxg.vertex_name[v]
+        vtext[v] = s
+
+dprops = gv.new_vertex_property('string')
+for v in gv.vertices():
+    st = str(list(taxg.vertex_strees[v]))
+    s = taxg.vertex_name[v]
+    if not s:
+        s = str([ taxg.taxid_name(x) for x in taxg.vertex_stem_cdef[v] ])
+    s = '%s: %s' % (st, s)
+    dprops[v] = s
+    
+pos, pin = tg.layout(taxg, gv, root, sfdp=True, deg0=190.0,
+                     degspan=160.0, radius=1000)
+
+gt.interactive_window(
+    gv, pos=pos, pin=True,
+    vertex_fill_color=vcolor,
+    vertex_text_position=3.1415,
+    vertex_text=vtext,
+    vertex_size=vsize,
+    vertex_pen_width=0,
+    edge_color=ecolor,
+    edge_pen_width=ewidth,
+    display_props=dprops,
+    update_layout=False
+    )
