6c97831a16609050093f6dbdb6edc5a4b11794c9
Rick Ree <github@rree.fastmail.fm>
2016-04-06 15:33:14 -0500

    conditional import of scipy.linalg in chars/mk.py

--- ivy/chars/__init__.py
+++ ivy/chars/__init__.py
@@ -1,1 +1,7 @@
-import mk, catpars, evolve
+import catpars, evolve
+try:
+    import mk
+except ImportError:
+    import sys
+    sys.stderr.write(
+        'Cannot import mk; discrete character models not available\n')
