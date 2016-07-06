0d2d3febcb5f97453d11a2e0ae3a27f292d97ba5
Christie Ziegler <chriziegler@gmail.com>
2016-07-06 13:13:41 -0500

    cyexpokit dependencies tweaked

--- ivy/chars/expokit/setup.py
+++ ivy/chars/expokit/setup.py
@@ -11,7 +11,7 @@ directive_defaults['binding'] = True
 ext = Extension(
     "cyexpokit",
     ["cyexpokit.pyx"],
-    libraries = ["lapack", "blas", "m"],
+    libraries = ["lapack", "blas", "m","pyFPE"],
     extra_objects = ["dexpm_c.o", "expokit.o", "mataid.o", "clock.o"],
     define_macros=[('CYTHON_TRACE', '1')],
     include_dirs=[numpy.get_include()]
